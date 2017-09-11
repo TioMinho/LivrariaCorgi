112eab78fd04d31815276151616a099e96bed2ee
Minho Menezes <minhotmog@gmail.com>
2017-09-10 23:37:14 -0300

    Added 'ConsultarDatabase' functionalities and fixed some bugs

--- livrariaCorgi.py
+++ livrariaCorgi.py
@@ -7,15 +7,15 @@ import os
 # Funções Auxiliares
 def getProduto(codigo):
 	nomeArquivo = "database/" + str(codigo)
-	file = file.open(nomeArquivo, 'r')
+	file = open(nomeArquivo, 'r')
 	
-	titulo 		= file.readline() - '\n'
-	autor 		= file.readline() - '\n'
-	descricao 	= file.readline() - '\n'
-	preco 		= int(file.readline() - '\n')
-	estoqueMax	= int(file.readline() - '\n')
-	custoRepo	= int(file.readline() - '\n')
-	estoque 	= int(file.readline() - '\n')
+	titulo 		= file.readline().rstrip()
+	autor 		= file.readline().rstrip()
+	descricao 	= file.readline().rstrip()
+	preco 		= int(file.readline().rstrip())
+	estoqueMax	= int(file.readline().rstrip())
+	custoRepo	= int(file.readline().rstrip())
+	estoque 	= int(file.readline().rstrip())
 
 	file.close()
 
@@ -25,7 +25,6 @@ def setProduto(codigo, titulo, autor, descricao, preco, estoqueMax, custoRepo, e
 	nomeArquivo = 'database/' + str(codigo)
 	file = open(nomeArquivo, 'w')
 	
-	file.write(str(codigo) + '\n')
 	file.write(str(titulo) + '\n')		
 	file.write(str(autor) + '\n')
 	file.write(str(descricao) + '\n')
@@ -93,10 +92,10 @@ if __name__=="__main__":
 				print("Estoque:", estoque)
 
 				if(estoque > 0):
-					confirma = input("Confirmar compra? (S - Sim | N - Não: ")
+					confirma = input("\nConfirmar compra? (S - Sim | N - Não: ")
 					confirma = confirma.lower()
 
-					if(confirma = 's'):
+					if(confirma == 's'):
 						receitaT = receitaT + precoUni
 						lucroT = lucroT + precoUni
 						estoque = estoque - 1
@@ -105,13 +104,45 @@ if __name__=="__main__":
 				else:
 					print("Estoque indisponível para esse produto!")
 
-
-
 		# Opção: Consultar Database
 		elif(comando == 2):
 			print("Aguarde...")
 			time.sleep(1)	
 			os.system("reset")
+
+			codigo = 1
+			while(codigo != 0 and qtdProdutos > 0):
+				time.sleep(1)	
+				os.system("clear")
+				print("###############    Vender Produto    ###############\n")
+				titulo, autor, descricao, precoUni, estoqueMax, custoRepo, estoque = getProduto(codigo)
+
+				print("Codigo:\t", codigo)
+				print("Título:\t", titulo)
+				print("Autor:\t", autor)
+				print("Descrição:\t", descricao)
+				print("Preço:\t", precoUni)
+				print("Estoque Máximo:\t", estoqueMax)
+				print("Custo de Reposição:\t", custoRepo)
+				print("Estoque:\t", estoque)
+
+				acao = input("\n(A - Anterior | P - Próximo | C - Pesquisar por Código | S - Voltar ao Menu): ")
+				acao = acao.lower()
+
+				if(acao == 'a' and codigo > 1):
+					codigo -= 1
+				elif(acao == 'p' and codigo < qtdProdutos):
+					codigo += 1
+				elif(acao == 'c'):
+					codigo = eval(input("Digite o Código para Pesquisar: "))
+					
+					if(codigo > qtdProdutos):
+						print("Código Inválido!")
+						codigo = 1
+
+				elif(acao == 's'):
+					print("Aguarde...")
+					codigo = 0
 
 		# Opção: Adicionar Produtos
 		elif(comando == 3):
@@ -129,13 +160,13 @@ if __name__=="__main__":
 			estoqueMax 	= input("Máximo de Estoque: ")
 			custoRepo 	= input("Custo de Reposição: ")
 
-			confirm = input("Adicionar este produto no Banco de Dados? (S - Sim | N - Não): ")
+			confirm = input("\nAdicionar este produto no Banco de Dados? (S - Sim | N - Não): ")
 			confirm = confirm.lower()
 			
 			if(confirm == 's'):
 				qtdProdutos = qtdProdutos + 1
 
-				setProduto(qtdProdutos, titulo, autor, descricao, precoUni, estoqueMax, custoRepo)
+				setProduto(qtdProdutos, titulo, autor, descricao, precoUni, estoqueMax, custoRepo, 0)
 
 				print("O produto foi adicionado no Banco de Dados com sucesso!")
 
